426a559d6fc89c52b65c44024bc5f8d8ce0122df
SauravTiwari1T <120175548+SauravTiwari1T@users.noreply.github.com>
2024-10-26 16:40:18 +0530

    Update 1.Select statement.sql

--- 1.Select statement.sql
+++ 1.Select statement.sql
@@ -27,3 +27,39 @@ VALUES
 (5, 'Charlie', 'Davis', 'IT', 70000.00, '2022-02-20');
 
 
+select * from Employess ;
+
+-- Examples:
+
+select EmployeeId , CONCAT(FirstName ,' ', LastName) AS FullName from Employess;
+-- Full name is called aliases 
+
+
+-- Created a new table 
+CREATE TABLE EmployeeRecords (
+    EmployeeID INT PRIMARY KEY,
+    FirstName NVARCHAR(50),
+    LastName NVARCHAR(50),
+    Department NVARCHAR(50),
+    Salary DECIMAL(10, 2)
+);
+
+
+INSERT INTO EmployeeRecords (EmployeeID, FirstName, LastName, Department, Salary)
+VALUES
+(1, 'John', 'Miller', 'HR', 60000),
+(2, 'Jane', 'Smith', 'Finance', 75000),
+(3, 'John', 'Miller', 'HR', 60000),
+(4, 'Emily', 'Davis', 'IT', 85000),
+(5, 'Michael', 'Brown', 'Finance', 75000),
+(6, 'Jane', 'Smith', 'Finance', 75000);
+
+
+select * from employeerecords;
+
+
+select Firstname  from employeerecords;
+
+-- to find distinct value 
+
+select distinct(firstname) from employeerecords;
