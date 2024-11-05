f1dbcb435e5127a523c2f3e806e13b983be018a8
SauravTiwari1T <120175548+SauravTiwari1T@users.noreply.github.com>
2024-11-05 20:56:20 +0530

    Update 2.Temporary_Table.sql

--- 2.Temporary_Table.sql
+++ 2.Temporary_Table.sql
@@ -2,3 +2,5 @@ select * into #temp1 from [dbo].[Employess];
 select * from #temp1;
 
 -- We have created a temporary table. The command `SELECT * INTO #temp1 FROM [dbo].[Employees];` copies all the data from the `[dbo].[Employees]` table into the temporary table `#temp1`. This table exists only temporarily and does not create a permanent table.
+# is used for local Temporary table 
+## is used for global Temporary table 
