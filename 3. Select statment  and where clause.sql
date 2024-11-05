18cb1cd736fd5710987fd86e94e7d072c91353e4
SauravTiwari1T <120175548+SauravTiwari1T@users.noreply.github.com>
2024-11-05 22:45:12 +0530

    Update 3. Select statment  and where clause.sql

--- 3. Select statment  and where clause.sql
+++ 3. Select statment  and where clause.sql
@@ -1,5 +1,5 @@
-# // The SELECT statement is used to retrieve table columns
-# // The * symbol means selecting all columns, but you can specify the names of columns to select only specific ones.
- #  //  The WHERE clause is used to apply a condition to filter the rows returned by the query. It can be used with column values, constants, or expressions, but aggregate functions are typically used with HAVING to filter based on aggregated data.
+-- The SELECT statement is used to retrieve table columns
+-- The * symbol means selecting all columns, but you can specify the names of columns to select only specific ones.
+-- The WHERE clause is used to apply a condition to filter the rows returned by the query. It can be used with column values, constants, or expressions, but aggregate functions are typically used with HAVING to filter based on aggregated data.
    select * from employees
 where EmployeeId = 2
