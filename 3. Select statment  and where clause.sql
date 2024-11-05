7be78e87b2074ece4fa4d67828622bbddd3d07f4
SauravTiwari1T <120175548+SauravTiwari1T@users.noreply.github.com>
2024-11-05 21:24:10 +0530

    Create 3. Select statment  and where clause.sql

---
+++ 3. Select statment  and where clause.sql
@@ -0,0 +1,3 @@
+# // The SELECT statement is used to retrieve table columns
+# // The * symbol means selecting all columns, but you can specify the names of columns to select only specific ones.
+  //  The WHERE clause is used to apply a condition to filter the rows returned by the query. It can be used with column values, constants, or expressions, but aggregate functions are typically used with HAVING to filter based on aggregated data.
